3892ae065a4d5b649c7d2a3aada9e49cb8d9015f
Pedro Rosanes <prosanes@gmail.com>
2014-02-01 14:17:44 -0200

    Create more tests

--- TDDMicroExercises/Ruby/tire_pressure_monitoring_system/alarm_test.rb
+++ TDDMicroExercises/Ruby/tire_pressure_monitoring_system/alarm_test.rb
@@ -2,14 +2,31 @@ require 'test/unit'
 require_relative './alarm'
 
 class AlarmTest < Test::Unit::TestCase
-	class SensorDouble
+	class SensorLowerThemLowPressure
 		def pop_next_pressure_psi_value
-			14
+			Alarm::LOW_PRESSURE - 5
 		end
 	end
 
-	def test_should_be_on_when_pressure_is_lower_then_lower_limit
-		alarm = Alarm.new(SensorDouble.new)
-		assert alarm.check, "Check should be true"
+	class SensorAlwaysLowPressure
+		def pop_next_pressure_psi_value
+			Alarm::LOW_PRESSURE
+		end
 	end
+
+	def test_should_be_off_when_initialized
+		alarm = Alarm.new SensorLowerThemLowPressure.new
+		assert !alarm.alarm_on 
+	end
+
+	def test_should_be_on_when_pressure_is_lower_then_lower_limit
+		alarm = Alarm.new SensorLowerThemLowPressure.new
+		assert alarm.check
+	end
+
+	def test_should_be_off_when_pressure_equals_lower_limit
+		alarm = Alarm.new SensorAlwaysLowPressure.new
+		assert !alarm.check 
+	end
+
 end
