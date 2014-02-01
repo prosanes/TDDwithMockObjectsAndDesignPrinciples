c930939a4c34a73cf2e1c33ba7e8d94cd41e6f88
Pedro Rosanes <prosanes@gmail.com>
2014-02-01 15:04:58 -0200

    Change test framework to RSpec

---
+++ TDDMicroExercises/Ruby/tire_pressure_monitoring_system/alarm_spec.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require_relative './alarm'
+
+describe Alarm, "check" do
+	it "should be off when initialized" do
+		alarm = Alarm.new nil
+		alarm.alarm_on.should be_false
+	end
+
+	it "should be on when pressure is lower then lower limit" do
+		alarm = Alarm.new create_sensor_double_with(pressure: Alarm::LOW_PRESSURE - 1)
+		alarm.check.should be_true
+	end
+
+	it "should be off when pressure equals lower limit" do
+		alarm = Alarm.new create_sensor_double_with(pressure: Alarm::LOW_PRESSURE)
+		alarm.check
+		alarm.alarm_on.should be_false
+	end
+	
+	def create_sensor_double_with(pressure: 0)
+		sensorDouble = double()
+		sensorDouble.stub(:pop_next_pressure_psi_value)
+					.and_return(pressure)
+		return sensorDouble
+	end
+end
+
