b1421101b9eb23969751995a0b4e6c9dbb026474
Pedro Rosanes <prosanes@gmail.com>
2014-02-08 17:39:26 -0200

    Isolated TicketDispenser from sequence generator to turn it into a UnitTest.

--- TDDMicroExercises/Ruby/turn_ticket_dispenser/ticket_dispenser_spec.rb
+++ TDDMicroExercises/Ruby/turn_ticket_dispenser/ticket_dispenser_spec.rb
@@ -1,15 +1,16 @@
 require_relative './ticket_dispenser'
 
 describe TicketDispenser do
+
 	context "When there is only one TicketDispenser" do
 		it "returns a TurnTicket with number 1" do
-			td = TicketDispenser.new
+			td = TicketDispenser.new(sequence_generator: SequenceGeneratorStub.new)
 			ticket = td.get_turn_ticket
 			expect(ticket.turn_number).to eq(1)
 		end
 
 		it "returns sequential TurnTicket numbers" do
-			td = TicketDispenser.new
+			td = TicketDispenser.new(sequence_generator: SequenceGeneratorStub.new)
 			ticket1_number = td.get_turn_ticket.turn_number
 			(1..3).each do
 				ticket2_number = td.get_turn_ticket.turn_number
@@ -19,15 +20,26 @@ describe TicketDispenser do
 		end
 	end
 
-	context "when there are multiple TicketDispenser" do
+	context "when there are multiple TicketDispenser using the same sequence generator" do
 		it "Multiple ticket dispensers dont return the same TurnTicket" do
-			td1 = TicketDispenser.new
-			td2 = TicketDispenser.new
+			sg = SequenceGeneratorStub.new
+			td1 = TicketDispenser.new(sequence_generator: sg)
+			td2 = TicketDispenser.new(sequence_generator: sg)
 			
 			ticket1_number = td1.get_turn_ticket.turn_number
 			ticket2_number = td2.get_turn_ticket.turn_number
 
 			expect(ticket1_number).to be < ticket2_number
 		end
+	end
+end
+
+class SequenceGeneratorStub
+	def initialize
+		@turn_number = 0
+	end
+
+	def get_next_turn_number
+		@turn_number += 1
 	end
 end
